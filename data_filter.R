dd7d94ce02246f4ac9b6ce385b0bbb6305d2f323
Pedro Firmino <pedrohefirmino@gmail.com>
2024-12-18 17:10:49 -0300

    Filter data for Poland

---
+++ data_filter.R
@@ -0,0 +1,39 @@
+
+#Filter for Poland
+install.packages("data.table")
+library(data.table)
+
+#Important - You must have change the directory
+input_file <- "F:/species-map/data/occurence.csv"
+output_file <- "F:/species-map/data/poland_data.csv"
+
+#Read
+cat("Reading the file...\n")
+data <- fread(input_file, colClasses = "character")
+
+#Verify the first few lines
+cat("First few lines of the file:\n")
+print(head(data))
+
+#Filter by 'country' column exists
+if (!"country" %in% colnames(data)) {
+  cat("The 'country' column was not found. The current columns are:", colnames(data), "\n")
+  stop("Process interrupted..")
+}
+
+#Filter the data for 'Poland'
+cat("Filtering data for 'Poland'\n")
+poland_data <- data[country == "Poland"]
+
+#Quantity of records were found
+cat("Total records for 'Poland':", nrow(poland_data), "\n")
+
+#Check if there is data to save
+if (nrow(poland_data) > 0) {
+  cat("Saving the filtered data...\n")
+  fwrite(poland_data, output_file)
+} else {
+  cat("No data for 'Poland' found.\n")
+}
+
+cat("Processing completed saved in:", output_file, "\n")
