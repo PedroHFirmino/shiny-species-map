b45130ffb376d4540a12e453aacf5fd1cec97a96
Pedro Firmino <pedrohefirmino@gmail.com>
2024-12-19 10:43:32 -0300

    Creating UI

--- App.R
+++ App.R
@@ -13,7 +13,7 @@ library(janitor)
 bio_occurrence <- read_csv("F:/species-map/data/poland_data.csv") #Important - You must have change the directory.
 
 bio_occurrence <- bio_occurrence %>%
-  select(id, scientificName, vernacularName, individualCount, lifeStage, sex, longitudeDecimal, latitudeDecimal,
+  select(id, scientificName, vernacularName, individualCount, lifeStage, longitudeDecimal, latitudeDecimal,
          locality, modified, taxonRank, family, kingdom  )
 
 bio_occurrence <- bio_occurrence %>%
@@ -27,3 +27,53 @@ bio_occurrence <- bio_occurrence %>%
 
 #Dropdown
 sv_names <- unique(bio_occurrence$names)
+
+
+#UI - Dashboard
+
+ui <- dashboardPage(
+  skin = "green",
+  dashboardHeader(
+    title = tags$div(
+      style = "display: flex; justify-content: center; align-items: center; flex-direction: column;",
+      tags$span("Biodiversity Dashboard by Pedro", style = "font-size: 18px;")
+    ),
+    titleWidth = '100%'
+  ),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Map of Species Occurrence in Poland", tabName = "bio_occurrence")
+    )
+  ),
+
+dashboardBody(
+  tabItems(
+    tabItem(
+      tabName = "bio_occurrence",
+      fluidRow(
+        column(
+          width = 4,
+          selectInput("species_selector", "Choose a specie:", choices = sv_names)
+        ),
+        column(
+          width = 8,
+          leafletOutput("occurrence_map",height = 400)
+        )
+      ),
+      fluidRow(
+        column(
+          width = 12,
+          h4("Map of Total Species"),
+          leafletOutput("total_map", height = 400)
+        )
+      )
+    )
+  )
+  )
+)
+
+
+
+
+shinyApp(ui=ui, server = server)
+
